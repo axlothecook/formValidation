3aedae4e77f0afddb24eccc6458a786b09fea38d
axlothecook <alliston.ruby123@gmail.com>
2025-02-06 19:16:42 +0100

    created files

---
+++ webpack.common.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/js/index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Production',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+   new HtmlWebpackPlugin({
+     template: "./src/template.html",
+   }),
+ ],
+ stats: {
+   loggingDebug: ["babel-loader"],
+ },
+ module: {
+   rules: [
+     {
+       test: /\.css$/i,
+       use: ["style-loader", "css-loader"],
+     },
+     {
+       test: /\.html$/i,
+       loader: "html-loader",
+     },
+     {
+       test: /\.(png|svg|jpg|jpeg|gif)$/i,
+       type: "asset/resource",
+     },
+     {
+       test: /\.(?:js|mjs|cjs)$/,
+       exclude: /node_modules/,
+       use: {
+         loader: 'babel-loader',
+         options: {
+           targets: "defaults",
+           presets: [
+             ['@babel/preset-env']
+           ]
+         }
+       }
+     },
+   ],
+ },
+};
